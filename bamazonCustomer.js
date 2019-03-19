0aa40bc84064918c651aeda30c0fbbbdbac51dc2
chaosZeroFive <chaos.five.zero@gmail.com>
2019-03-18 21:32:04 -0500

    fixed query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,11 +27,11 @@ function start() {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             table.push([
-                res[i].item_id,
-                res[i].product_name,
-                res[i].department_name,
-                res[i].product_price,
-                res[i].stock_quantity
+                res[i].id,
+                res[i].product,
+                res[i].department,
+                res[i].price,
+                res[i].quantity
             ]);
         }
         console.log(table.toString());
@@ -77,29 +77,29 @@ function purchase() {
 }
 
 function checkOut(id, qty) {
-    let query = "SELECT * FROM products WHERE item_id = " + id + ";";
+    let query = "SELECT * FROM products WHERE id = " + id + ";";
     connection.query(query, function (err, res) {
         if (err) throw err;
         else {
             for (var i = 0; i < res.length; i++) {
-                if (qty <= res[i].stock_quantity) {
-                    let prod = res[i].product_name;
-                    let dep = res[i].department_name;
-                    let price = res[i].product_price;
+                if (qty <= res[i].quantity) {
+                    let price = res[i].price.toFixed(2);
                     let cost = price * qty;
                     let newSale = new Map();
-                    newSale.set(0, res[i].item_id);
-                    newSale.set(1, res[i].product_name);
-                    newSale.set(2, res[i].department_name);
-                    newSale.set(3, res[i].product_price);
-                    newSale.set(4, res[i].stock_quantity);
+                    
+                    newSale.set(0, res[i].id);
+                    newSale.set(1, res[i].product);
+                    newSale.set(2, res[i].department);
+                    newSale.set(3, price);
+                    newSale.set(4, res[i].quantity);
                     newSale.set(5, cost);
                     newSale.set(6, qty);
+
                     console.log(`
                     ${chalk.yellow("Your total is: $" + cost)}
                     ${chalk.yellow("Thank You for your purchase!")}
                     `);
-                    console.log("From checkOut " + newSale);
+                    
                     inquirer.prompt([{
                         type: "list",
                         name: "action",
@@ -114,19 +114,8 @@ function checkOut(id, qty) {
                     }]).then(answers => {
                         managment(newSale);
                         manageSales(newSale);
-                        let c = answers.action.toString();
-                        switch (c) {
-                            case "continue shopping":
-                                start();
-                                break;
-                            case "exit":
-                                exitBamazon();
-                                break;
-                        }
+                        
                     });
-                }
-                else {
-                    console.log("Removed Message");
                 }
             }
         }
@@ -135,35 +124,38 @@ function checkOut(id, qty) {
 
 function managment(newSale) {
     let id = newSale.get(0);
-    let product = newSale.get(1);
-    let department = newSale.get(2);
-    let price = newSale.get(3);
-    let quantity = newSale.get(4);
+    console.log("From managment() " + id);
     let qty = newSale.get(6);
-    let query = "UPDATE products SET stock_quantity = stock_quantity - " + qty + ";";
+    console.log("From managment() " + qty);
+    let query = "UPDATE products SET quantity = quantity - " + qty + " WHERE id = " + id + ";";
     console.log(query);
     connection.query(query, function (err, res) {
         if (err) throw err;
 
         else {
-            console.log(product + "inventory has been updated");
+            console.log("inventory has been updated");
         }
     })
 }
 
 function manageSales(newSale) {
-    console.log("From manageSales " + newSale);
-    let id = newSale.get(0);
-    let product = "'" + newSale.get(1); + "'";
-    let department = "'" + newSale.get(2); + "'";
+
+    let product = "'" + newSale.get(1) + "'";
+    console.log("From products " + product);
+    let department = "'" + newSale.get(2) + "'";
+    console.log("From department " + department);
     let price = newSale.get(3);
+    console.log("From price " + price);
     let quantity = newSale.get(4);
-    let cost = newSale[5];
-    let qty = newSale[6];
-    let insert = "INSERT INTO sales ( trans_total, trans_department, trans_product, trans_quantity, unit_price, unit_on_hand ) ";
-    let values = "VALUES ( " + cost + ", " + department + ", " + product + ", " + quantity + ", " + price + ", " + qty + " );";
+    console.log("From quantity " + quantity);
+    let cost = newSale.get(5);
+    console.log("From cost " + cost);
+    let qty = newSale.get(6);
+    console.log("From qty " + qty);
+    let insert = "INSERT INTO sales ( trans_product, trans_total, trans_department, unit_price, unit_total ) ";
+    let values = "VALUES ( " + product + ", " + cost + ", " + department + ", " + price + ", " + qty + " );";
     let query = insert + values;
-    console.log(query);
+    console.log("From query " + query);
     connection.query(query, function (err, res) {
         if (err) throw err;
 
